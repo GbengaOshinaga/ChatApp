4ab85cbd58e00884ab5b3aeda5987438e0fbb567
Gbenga Oshinaga <andeladeveloper@Andelas-MacBook-Pro.local>
2018-09-13 13:03:00 +0100

    feat(logout): add logout functionality

--- src/screens/Chat/index.js
+++ src/screens/Chat/index.js
@@ -1,9 +1,38 @@
+/* eslint-disable react/forbid-prop-types */
+
 import React from 'react';
-import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import { Button, Icon } from 'native-base';
+import {
+  View, Text, AsyncStorage, Platform,
+} from 'react-native';
+
+const propTypes = {
+  navigation: PropTypes.any.isRequired,
+};
 
 class ChatScreen extends React.Component {
-  static navigationOptions = {
+  static navigationOptions = ({ navigation }) => ({
     title: 'ChatApp',
+    headerRight: (
+      <Button
+        transparent
+        light
+        style={{ paddingRight: 10 }}
+        onPress={navigation.getParam('logOut')}
+      >
+        <Icon name={Platform.OS === 'ios' ? 'ios-log-out' : 'md-log-out'} />
+      </Button>
+    ),
+  })
+
+  componentDidMount() {
+    this.props.navigation.setParams({ logOut: this.logOut });
+  }
+
+  logOut = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate('Auth');
   }
 
   render() {
@@ -14,5 +43,7 @@ class ChatScreen extends React.Component {
     );
   }
 }
+
+ChatScreen.propTypes = propTypes;
 
 export default ChatScreen;
